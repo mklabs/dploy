32d7a14ac6cf3045e0646fa535854687a1c541c3
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-14 16:33:14 +0100

    test - a .travis.yml file and some cleanup

--- test/test-s3-request.js
+++ test/test-s3-request.js
@@ -7,9 +7,10 @@ var S3 = require('../').S3;
 var auth, client;
 
 try {
-  auth = require(path.join(__dirname, '../auth'));
+  auth = require(path.join(__dirname, '../auth.json'));
   client = new S3(auth.s3);
 } catch (err) {
+	console.log(err);
   console.error('`npm test` requires ./auth.json to contain a JSON string with');
   console.error('s3.key, s3.secret, and s3.bucket in order to run tests.');
   process.exit(1);
