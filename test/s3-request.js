f8522b7c006b16c8e68195e8f29eec195fc0b11a
Mickael Daniel <daniel.mickael@gmail.com>
2012-03-14 16:46:00 +0100

    test - just basic api tests for `npm test`
    should trigger auth-dependant tests manually

--- test/s3-request.js
+++ test/s3-request.js
@@ -10,7 +10,6 @@ try {
   auth = require(path.join(__dirname, '../auth.json'));
   client = new S3(auth.s3);
 } catch (err) {
-	console.log(err);
   console.error('`npm test` requires ./auth.json to contain a JSON string with');
   console.error('s3.key, s3.secret, and s3.bucket in order to run tests.');
   process.exit(1);
